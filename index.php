ed73eda6bd466710c9e19ee05f1c252133300138
Drew0004 <drdrewgraphic@gmail.com>
2024-02-06 15:47:30 +0100

    Completato esercizio di base

--- index.php
+++ index.php
@@ -38,6 +38,7 @@
             'distance_to_center' => 50
         ],
 
+
     ];
 
 ?>
@@ -51,9 +52,49 @@
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
-    <body>
-        
-
+    <body class="bg-dark">
+        <div class="container">
+            <h1 class="text-white text-center my-4">
+                Hotels
+            </h1>
+    
+            <table class="table border mt-4">
+                <thead>
+                    <tr>
+                        <?php  
+                           
+                            $keys = array_keys($hotels[0]);
+    
+                            foreach($keys as $key) {
+                                echo '<th scope="col">'.$key.'</th>';
+    
+                            }
+                            
+                        ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                        foreach ($hotels as $singleHotel){;
+                    ?>
+                    <tr>
+                        <?php 
+                            echo '<td>'.$singleHotel['name'].'</td>';
+                            echo '<td>'.$singleHotel['description'].'</td>';
+                            echo '<td>'.$singleHotel['parking'].'</td>';
+                            echo '<td>'.$singleHotel['vote'].'</td>';
+                            echo '<td>'.$singleHotel['distance_to_center'].'</td>';
+                        ?>
+                            
+    
+                            
+                    </tr>
+                    <?php  
+                        };
+                    ?>   
+                </tbody>
+            </table>
+        </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
